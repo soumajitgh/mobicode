b96fcf0087137064f4afa20264cdfcea3a4fd94b
Soumajit Ghosh <soumojitghosh02@gmail.com>
2026-08-17 23:33:05 +0530

    refactor(api)!: remove REST user routes
    GraphQL is the sole user transport.
    
    BREAKING CHANGE: /users endpoints are no longer available; use /query.

--- internal/user/module.go
+++ internal/user/module.go
@@ -1,15 +1,9 @@
 package user
 
-import (
-	"github.com/go-chi/chi/v5"
-	"go.uber.org/fx"
-)
+import "go.uber.org/fx"
 
-// Module provides the user feature and registers its routes.
+// Module provides the user persistence and business-logic dependencies.
 var Module = fx.Module(
 	"user",
-	fx.Provide(NewRepository, NewService, NewHandler),
-	fx.Invoke(func(mux *chi.Mux, handler *Handler) {
-		handler.RegisterRoutes(mux)
-	}),
+	fx.Provide(NewRepository, NewService),
 )
